acb4ae6477279442acc7c70a737d8513797c2e4c
neupane857 <neupane857@gmail.com>
2023-11-30 04:31:25 -0600

    RecognizeSound Class update

--- app/src/main/java/com/example/soundrecognitionofanimals/RecognizeSound.java
+++ app/src/main/java/com/example/soundrecognitionofanimals/RecognizeSound.java
@@ -1,6 +1,7 @@
 package com.example.soundrecognitionofanimals;
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.view.View;
@@ -8,14 +9,31 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import android.widget.Toast;
+import android.util.Log;
+import java.util.ArrayList;
+import java.util.List;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
+
+
+import java.io.File;
 import java.io.IOException;
 
+
 public class RecognizeSound extends AppCompatActivity {
-    private Button buttonRecord;
+    private Button buttonRecord, buttonPlay, buttonBack;
+    private Spinner spinnerRecordings;
     private MediaRecorder recorder;
+    private MediaPlayer player;
     private String fileName = null;
-    private boolean isRecording = false;
+    private boolean isRecording = false, isPlaying = false;
+    private List<String> recordedFiles = new ArrayList<>();
+    private ArrayAdapter<String> recordingsAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,8 +41,21 @@ public class RecognizeSound extends AppCompatActivity {
         setContentView(R.layout.activity_recognizesound);
 
         buttonRecord = findViewById(R.id.buttonRecord);
-        fileName = getExternalCacheDir().getAbsolutePath();
-        fileName += "/audiorecordtest.3gp";
+        buttonPlay = findViewById(R.id.buttonPlay); // Assuming you have a buttonPlay in your layout
+        buttonBack = findViewById(R.id.buttonBack);
+        spinnerRecordings = findViewById(R.id.spinnerRecordings);
+
+        recordingsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item);
+        recordingsAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerRecordings.setAdapter(recordingsAdapter);
+
+        // Check if the RecordedSound folder exists, if not, create it
+        File folder = new File(getExternalFilesDir(null), "RecordedSound");
+        if (!folder.exists()) {
+            folder.mkdirs(); // This will create the folder if it does not exist
+        }
+
+        fileName = new File(folder, "audiorecordtest.3gp").getAbsolutePath();
 
         buttonRecord.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,9 +73,36 @@ public class RecognizeSound extends AppCompatActivity {
                 }
             }
         });
+
+        buttonPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isPlaying) {
+                    stopPlaying();
+                    buttonPlay.setText("Play");
+                    isPlaying = false;
+                } else {
+                    startPlaying();
+                    buttonPlay.setText("Stop");
+                    isPlaying = true;
+                }
+            }
+        });
+
+        buttonBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish(); // Closes the current activity and returns to the previous one
+            }
+        });
     }
 
     private void startRecording() {
+        // Use a timestamp to create a unique file name for each recording
+        String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(new Date());
+        fileName = new File(getExternalFilesDir(null) + "/RecordedSound/", "AUD_" + timestamp + ".3gp").getAbsolutePath();
+        recordedFiles.add(fileName); // Add file name to the list
+
         recorder = new MediaRecorder();
         recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
         recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
@@ -53,17 +111,73 @@ public class RecognizeSound extends AppCompatActivity {
 
         try {
             recorder.prepare();
+            recorder.start();
+        } catch (IOException e) {
+            Log.e("RecognizeSound", "Recording start failed", e);
+            Toast.makeText(this, "Failed to start recording", Toast.LENGTH_SHORT).show();
+        }
+        updateRecordingsList();
+    }
+
+    private void playSelectedRecording() {
+        int selectedPosition = spinnerRecordings.getSelectedItemPosition();
+        if (selectedPosition > 0 && selectedPosition <= recordedFiles.size()) {
+            String selectedFile = recordedFiles.get(selectedPosition - 1); // Adjust for prompt
+            playRecording(selectedFile);
+        } else {
+            Toast.makeText(this, "Please select a recording", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+
+    private void playRecording(String filePath) {
+        if (player == null) {
+            player = new MediaPlayer();
+        }
+
+        try {
+            player.reset(); // Reset the MediaPlayer to its uninitialized state
+            player.setDataSource(filePath);
+            player.prepare();
+            player.start();
+        } catch (IOException e) {
+            Log.e("RecognizeSound", "Could not start playback", e);
+            Toast.makeText(this, "Playback failed", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+
+    private void stopRecording() {
+        try {
+            recorder.stop();
+            recorder.release();
+            recorder = null;
+            Toast.makeText(this, "Recording saved successfully", Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            // Log the error for debugging purposes
+            Log.e("RecognizeSound", "Failed to save the recording", e);
+            // Show a user-friendly error message
+            Toast.makeText(this, "Failed to save the recording", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    private void startPlaying() {
+        player = new MediaPlayer();
+        try {
+            player.setDataSource(fileName);
+            player.prepare();
+            player.start();
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        recorder.start();
     }
 
-    private void stopRecording() {
-        recorder.stop();
-        recorder.release();
-        recorder = null;
+    private void stopPlaying() {
+        player.release();
+        player = null;
     }
 
     private boolean checkPermissions() {
@@ -80,12 +194,19 @@ public class RecognizeSound extends AppCompatActivity {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == 1) {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                // Permission was granted
                 startRecording();
-            } else {
-                // Permission was denied
             }
         }
+    }
+    private void updateRecordingsList() {
+        recordingsAdapter.clear();
+        recordingsAdapter.add("Select a recording"); // Placeholder item
+
+        for (String filePath : recordedFiles) {
+            File file = new File(filePath);
+            recordingsAdapter.add(file.getName());
+        }
+        recordingsAdapter.notifyDataSetChanged();
     }
 
 }
